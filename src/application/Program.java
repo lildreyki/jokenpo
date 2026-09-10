7d3bee5e02aff3e31857ae6630d1a4333c054f18
lildreyki <lildreyki@icloud.com>
2026-09-09 22:55:22 -0300

    09/09/2026-22:55

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,28 @@
 package application;
 
+import entities.JoKenPo;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Program {
+
+    void main() {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        JoKenPo joKenPo = new JoKenPo();
+
+        joKenPo.showHome();
+        do {
+            joKenPo.showOptions();
+            joKenPo.setPc(random.nextInt(joKenPo.getOptions().length));
+            joKenPo.setPlayer(scanner.nextInt());
+            joKenPo.showWin();
+            joKenPo.nextGameQuestion();
+            joKenPo.setNextGame(scanner.nextBoolean());
+        } while (joKenPo.getNextGame() == true);
+        joKenPo.showHome();
+        scanner.close();
+    }
 }
--- src/entities/JoKenPo.java
+++ src/entities/JoKenPo.java
@@ -1,4 +1,93 @@
 package entities;
 
 public class JoKenPo {
+
+    private String[] options = new String[]{"Rock", "Paper", "Scissors"};
+    private Integer pc;
+    private Integer player;
+    private Boolean nextGame;
+
+    public JoKenPo(){}
+    public JoKenPo(String[] options, Integer pc, Integer player, Boolean nextGame) {
+        this.options = options;
+        this.pc = pc;
+        this.player = player;
+        this.nextGame = nextGame;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public void setOptions(String[] options) {
+        this.options = options;
+    }
+
+    public Integer getPc() {
+        return pc;
+    }
+
+    public void setPc(Integer pc) {
+        this.pc = pc;
+    }
+
+    public Integer getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Integer player) {
+        this.player = player;
+    }
+
+    public Boolean getNextGame() {
+        return nextGame;
+    }
+
+    public void setNextGame(Boolean nextGame) {
+        this.nextGame = nextGame;
+    }
+
+    public void showHome() {
+        IO.println("\n***************");
+        IO.println("*** JOKENPO ***");
+        IO.println("***************");
+    }
+
+    public void showOptions() {
+        IO.println();
+        int i = 0;
+        for (String o : options) {
+            IO.println("["+i+"] "+o);
+            i++;
+        }
+        IO.print(">>> ENTER: ");
+    }
+
+    public void showWin() {
+        IO.println("\n*** PLAYER: " + options[player]);
+        IO.println("*** PC: " + options[pc]);
+        win();
+    }
+
+    private void win() {
+        if (pc == 0 && player == 1) {
+            IO.println("*** PLAYER WON");
+        } else if (pc == 0 && player == 2) {
+            IO.println("*** PC WON");
+        } else if (pc == 1 && player == 0) {
+            IO.println("*** PC WON");
+        } else if (pc == 1 && player == 2) {
+            IO.println("*** PLAYER WON");
+        } else if (pc == 2 && player == 0) {
+            IO.println("*** PLAYER WON");
+        } else if (pc == 2 && player == 1) {
+            IO.println("*** PC WON");
+        } else {
+            IO.println("*** DRAW");
+        }
+    }
+
+    public void nextGameQuestion() {
+        IO.print("\n>>> PLAY AGAIN? (true/false): ");
+    }
 }
